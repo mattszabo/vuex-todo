c03ae08fb8371373819774d28907d7897f073ad0
Matt Szabo <szabo.matthew@gmail.com>
2016-10-07 15:25:04 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# vuex-todo
+
+> Another todo app, but this time it's written in Vue and uses Vuex
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ src/components/CompletedTodos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="completed-todos">
+    <h3 v-if="completed.length > 0">Completed ({{ completed.length }})</h3>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="todo in completed">
+        {{ todo.body }}
+        <button type="button" @click="remove(todo)" class="btn btn-danger btn-sm">
+          <span class="glyphicon glyphicon-remove-circle" />
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      retmove(todo) {
+        this.$store.dispatch('removeTodo', todo)
+      }
+    },
+    computed: {
+      completed() {
+        return this.$store.getters.completedTodos
+      }
+    }
+  }
+</script>
+
+<style>
+  .btn {
+    float: right;
+  }
+</style>
